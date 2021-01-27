054e5f9cf1490ed159765728e8185a33d7e78fe8
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-01-27 05:36:11 +0500

    add
    add

---
+++ js/index.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+
+    $("#menu-btn-bars").click(function(e) {
+        e.preventDefault();
+        $("#mob-menu").slideDown("slow");
+    });
+    $("#menu-btn-times").click(function(e) {
+        e.preventDefault();
+        $("#mob-menu").slideUp("slow");
+    });
+
+
+
+});
